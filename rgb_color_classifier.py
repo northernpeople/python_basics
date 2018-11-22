67285f3c8c096cd442122e2a5e1e5a19e516fa0b
Stepan Razdobarov <northernpeople@gmail.com>
2018-11-22 14:59:15 -0600

    reversed sort

--- rgb_color_classifier.py
+++ rgb_color_classifier.py
@@ -12,11 +12,11 @@ def find_nearest_neighbor(rgb_tuple):
     distances = {}
     for i in memory:
         distances[i] = distance(i, rgb_tuple)
-
-    sorted_distances = sorted(distances.items(), key=lambda t: t[1], reverse=True)
+    sorted_distances = sorted(distances.items(), key=lambda t: t[1])
+    print sorted_distances
     return memory[sorted_distances[0][0]]
 
-
+# 3 dimensional distance between tuples
 def distance(t1, t2):
     x_diff = t1[0] - t2[0]
     y_diff = t1[1] - t2[1]
@@ -25,11 +25,12 @@ def distance(t1, t2):
 
 
 values = [(0, 0, 0), (255, 0, 0), (255, 127, 80), (220, 20, 60), (0, 255, 255), (169, 169, 169), (255, 140, 0),
-          (255, 140, 0), (255, 140, 0)]
-labels = ["black", "red", "coral", "crimson", "cyan", "dark grey", "dark orange", "deep pink", "fuchsia"]
+          (255, 140, 0), (255, 140, 0), (255,99,71), (64,224,208), (0,255,127), (255,255,255)]
+labels = ["black", "red", "coral", "crimson", "cyan", "dark grey", "dark orange", "deep pink", "fuchsia", "tomato", "Turquoise", "spring green", "white"]
 
 train(values, labels)
 
-print find_nearest_neighbor((204, 45, 99))
 
-print memory
+print find_nearest_neighbor((255, 102, 102))
+
+# print memory
